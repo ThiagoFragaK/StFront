abfce687909e3ba1fabfd3ab2d5effe30ebbcbc3
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-15 20:27:36 -0300

    Name Change

--- pages/games.vue
+++ pages/games.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-    name: 'ErrorPage',
+    name: 'Games',
     data() {
         return {
         items: [
@@ -17,7 +17,7 @@ export default {
             { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
             { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
             { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
+          ]
         }
     }
 }
