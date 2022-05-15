5607a00ab89dcb3cedafec10b3553c4a06674c7c
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-15 13:56:28 -0300

    Creation of Error-Page

--- pages/error.vue
+++ pages/error.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>Error Page!</h1>
+  <b-container>
+    <br/>
+    <b-card
+      overlay
+      img-src="https://picsum.photos/900/250/?image=3"
+      img-alt="Card Image"
+    >
+      <b-card-text>
+        <h1>Error Page.</h1>
+        Some quick example text to build on the card and make up the bulk of the card's content.
+      </b-card-text>
+    </b-card>
+  </b-container>
 </template>
 
 <script>
