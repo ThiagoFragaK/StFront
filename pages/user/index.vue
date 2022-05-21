2c71cb0aa4bda03fc6f23963a9a8e9bacbcbb021
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-21 10:05:26 -0300

    UserPage with Components

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -2,106 +2,31 @@
   <b-container>
     <b-row align-v="center" class="justify-content-md-center mt-3">
       <b-col lg="12">
-        <b-card no-body class="overflow-hidden" style="max-height: 250px;">
-          <b-row no-gutters>
-            <b-col md="6">
-              <b-card-img :src="userInfo.avatarfull" alt="Image" class="rounded-0"></b-card-img>
-            </b-col>
-            <b-col md="6">
-              <b-card-body :title="userInfo.personaname">
-                <b-card-text>{{ userInfo.realname }}</b-card-text>
-                <br/>
-                <b-button :href="userInfo.profileurl" variant="outline-primary">Steam Profile</b-button>
-              </b-card-body>
-            </b-col>
-          </b-row>
-        </b-card>
+        <main-card />
       </b-col>
     </b-row>
     <b-row align-v="center" class="justify-content-md-center mt-3">
-      <b-col lg="4" md="3">
-        <b-card
-          title="Card Title"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mx-auto"
-        >
-          <b-card-text>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </b-card-text>
-
-          <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card> 
-      </b-col>
-      <b-col lg="4" md="3">
-        <div>
-          <b-card
-            title="Card Title"
-            img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mx-auto"
-          >
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>    
-      </b-col>
-      <b-col lg="4" md="3">
-        <div>
-          <b-card
-            title="Card Title"
-            img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mx-auto"
-          >
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>    
-      </b-col>
+      <recent-games-card />
     </b-row>
   </b-container>
 </template>
 
 <script>
+import MainCard from '../../components/user/MainCard.vue';
+import RecentGamesCard from '../../components/user/RecentGamesCard.vue';
+
 export default {
+  components: { MainCard, RecentGamesCard },
   name: 'UserPage',
   data(){
     return {
-      userInfo: {
-        avatarfull: null,
-        personaname: null,
-        realname: null,
-        profileurl: null
-      } 
     };
   },
   created() {
-    this.getData();
   },
   methods: {
     getData(){
-      this.$axios.$get('v1/user')
-				.then((response) => {
-          this.userInfo = response;
-          console.log(this.userInfo)
-				}
-      )
+     
     }
   }
 }
