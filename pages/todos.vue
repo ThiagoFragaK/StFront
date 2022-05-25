3a1c1f953051d29109ef2ecdada28289abaecb9d
ThiagoFragaK <thiago.fraga@fixsi.com.br>
2022-05-25 17:01:13 -0300

    Added: Todo Page

---
+++ pages/todos.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container>
+    <br/>
+    <h2>To-do List</h2>
+    <hr>
+    <div>
+      <b-dropdown split text="Choose a Game" variant="primary" class="m-md-2">
+        <b-dropdown-item>First Action</b-dropdown-item>
+      </b-dropdown>
+    </div>
+    <br/>
+    <todo-table />
+  </b-container>
+</template>
+
+<script>
+import TodoTable from '../components/todo/TodoTable.vue'
+
+export default {
+  components: { TodoTable },
+    name: 'Games',
+    data() {
+        return {
+        }
+    }
+}
+</script>
