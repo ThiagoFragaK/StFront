e2622d4b827ff392844b3487b008a1cd3daf7a6c
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-25 22:39:30 -0300

    Added: Acheivements in Game Page

---
+++ pages/game/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-container>
+    <b-row align-v="center" class="justify-content-md-center mt-3">
+      <b-row align-v="center" class="justify-content-md-center mt-3">
+        <b-col lg="6">
+            <game-info />
+        </b-col>
+
+        <b-col lg="12">
+          <br/>
+        </b-col>
+        
+        <b-col lg="12" >
+            <achievements />
+        </b-col>
+      </b-row>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Achievements from '../../components/game/Achievements.vue';
+import GameInfo from '../../components/game/GameInfo.vue';
+
+let tableColumns = [
+	{ key: "image", label: "", class: "text-center" },
+	{ key: "gameName", label: "Name", class: "text-center" },
+	{ key: "playTime", label: "Name", class: "text-center" },
+  { key: "gameId", label: "", class: "text-center col-1" }
+];
+
+export default {
+    components: { Achievements, GameInfo },
+    name: "MyGamesPage",
+    data(){
+        return {
+        };
+    },
+    created() {
+    },
+    methods: {
+    }
+};
+</script>
