feb12487d201b33d14e4d5e24b3429105d627713
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-25 22:38:50 -0300

    Added: Achievements Table Component

---
+++ components/game/Achievements.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-container>
+    <div>
+        <h1>Cheevos!</h1>
+    </div>
+  </b-container>
+</template>
+
+<script>
+
+export default {
+  name: "Achievements Page",
+  data() {
+    return {
+
+    }
+  },
+  created() {
+  },
+  methods: {
+  },
+};
+</script>
