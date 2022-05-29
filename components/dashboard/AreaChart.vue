200ef94af434b688429ecc3c97e47746948461eb
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-29 15:53:49 -0300

    Add: Default Graph Components

---
+++ components/dashboard/AreaChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-container>
+    <VueApexCharts
+      max-width="300"
+      type="area"
+      :options="options"
+      :series="series"
+    ></VueApexCharts>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      series: [
+        {
+          name: "Net Profit",
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+        },
+      ],
+      options: {
+        chart: {
+          type: "area",
+          height: 350,
+        },
+        xaxis: {
+          categories: [
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+          ],
+        },
+      },
+    };
+  },
+};
+</script>
