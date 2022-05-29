941c5546ebd908bfe3b3ff7e66f978ade9a0209e
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-29 15:53:57 -0300

    Add: Dummy graphs

--- pages/index.vue
+++ pages/index.vue
@@ -1,124 +1,31 @@
 <template>
   <b-container fluid="lg">
     <b-row align-v="center" class="justify-content-md-center mt-3">
-      <b-col lg="4" md="3">
-        <b-card
-          title="Card Title"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mx-auto"
-        >
-          <b-card-text>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </b-card-text>
-
-          <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card> 
+      <b-col lg="6" md="12"> 
+        <bar-chart />
       </b-col>
-      <b-col lg="4" md="3">
-        <div>
-          <b-card
-            title="Card Title"
-            img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mx-auto"
-          >
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>    
-      </b-col>
-      <b-col lg="4" md="3">
-        <div>
-          <b-card
-            title="Card Title"
-            img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mx-auto"
-          >
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>    
+      <b-col lg="6" md="12"> 
+        <bar-chart />
       </b-col>
     </b-row>
     <b-row align-v="center" class="justify-content-md-center mt-3">
-      <b-col lg="4" md="3">
-        <b-card
-          title="Card Title"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mx-auto"
-        >
-          <b-card-text>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </b-card-text>
-
-          <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card> 
+      <b-col lg="6" md="12"> 
+        <area-chart />
       </b-col>
-      <b-col lg="4" md="3">
-        <div>
-          <b-card
-            title="Card Title"
-            img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mx-auto"
-          >
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>    
-      </b-col>
-      <b-col lg="4" md="3">
-        <div>
-          <b-card
-            title="Card Title"
-            img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mx-auto"
-          >
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>    
+      <b-col lg="6" md="12"> 
+        <line-chart />
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import BarChart from '../components/dashboard/BarChart.vue'
+import AreaChart from '../components/dashboard/AreaChart.vue'
+import LineChart from '../components/dashboard/LineChart.vue'
+
 export default {
+  components: { BarChart, AreaChart, LineChart },
   name: 'IndexPage'
 }
 </script>
