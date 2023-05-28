3c7943eb3f8394f94cccd28be74623ef90263b39
ThiagoFragaK <thiago@fragainformatica.com>
2023-05-28 08:06:58 -0300

    Add: LoginForm Component

---
+++ components/main_page/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-card 
+        border-variant="primary" 
+        header="Login" 
+        class="text-center"
+    >
+        <b-card-text>
+            <div class="form-group">
+                <label for="username" class="form-label mt-2">Username</label>
+                <input disabled class="form-control" id="username" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <label for="password" class="form-label mt-2">Password</label>
+                <b-input-group>
+                    <input disabled type="password" class="form-control" id="password" placeholder="Password">
+                    <!-- <b-input-group-append>
+                        <b-button disabled variant="outline-primary">Show</b-button>
+                    </b-input-group-append> -->
+                </b-input-group>
+            </div>
+            <b-row>
+                <b-col>
+                    <div>
+                        <b-button disabled variant="outline-primary">Login</b-button>
+                    </div>
+                </b-col>
+                <b-col>
+                    <div>
+                        <b-button disabled variant="outline-success">Register</b-button>
+                    </div>
+                </b-col>
+            </b-row>
+        </b-card-text>
+    </b-card>
+</template>
