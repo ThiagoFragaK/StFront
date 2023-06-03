57ba4a6e72ba820e04358db67c7797ca89fb4019
ThiagoFragaK <thiago@fragainformatica.com>
2023-06-03 11:16:50 -0300

    Refactor: Set reload botton to active table

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -6,12 +6,12 @@
       </b-col>
     </b-row>
     <b-tabs align="center" content-class="mt-3" class="mt-4">
-      <b-tab title="Recent games" active>
+      <b-tab title="Recent games" active @click="setRecentGamesTable()">
         <b-row align-v="center" class="justify-content-md-center mt-3">
           <recent-games-table :steamID="steamID" ref="RecentGamesTable" />
         </b-row>
       </b-tab>
-      <b-tab title="My games">
+      <b-tab title="My games" @click="loadGamesTable()">
         <b-row align-v="center" class="justify-content-md-center mt-3">
           <players-games-table :steamID="steamID" ref="PlayersGamesTable"/>
         </b-row>
@@ -32,10 +32,27 @@ export default {
     RecentGamesTable,
     MainCard, 
   },
+  data() {
+    return {
+      isRecentGames: true,
+    };
+  },
   methods: {
-    reloadTable(){
-      this.$refs.RecentGamesTable.getRecentGames();
-    }
+    reloadTable() {
+      if(this.isRecentGames) {
+        this.$refs.RecentGamesTable.getRecentGames();
+      } else {
+        this.$refs.PlayersGamesTable.getGames();
+      }
+    },
+    loadGamesTable() {
+      this.isRecentGames = false;
+      this.$refs.PlayersGamesTable.getGames();
+    },
+    setRecentGamesTable() {
+      this.isRecentGames = true;
+    },
+
   },
   computed: {
     steamID() {
