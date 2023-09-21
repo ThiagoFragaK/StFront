dd8b6c2f2340d774fcf96845263eabd577197906
ThiagoFragaK <thiago@fragainformatica.com>
2023-09-20 22:18:53 -0300

    Add: Calculator Card Component

---
+++ components/calculator/CalculatorCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-container>
+        <b-row
+            align-v="center"
+            class="justify-content-md-center mt-3"
+        >
+            <h1>CALCULATOR CARD</h1>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    props: {
+    },
+    data() {
+        return {
+            config: {
+            },
+        };
+    },
+    methods: {
+    },
+    computed: {
+    },
+    created() {
+    },
+};
+</script>
