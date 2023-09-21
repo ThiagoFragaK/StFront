59c7851b96bdfa8f68b0b46c9ec28f6acaa643be
ThiagoFragaK <thiago@fragainformatica.com>
2023-09-20 22:18:40 -0300

    Add: Calculator tab

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -6,6 +6,11 @@
             </b-col>
         </b-row>
         <b-tabs align="center" content-class="mt-3" class="mt-4">
+            <b-tab title="Calculator" @click="loadGamesTable()">
+                <b-row align-v="center" class="justify-content-md-center mt-3">
+                    <calculator-card ref="CalculatorCard"/>
+                </b-row>
+            </b-tab>
             <b-tab title="Recent games" active @click="setRecentGamesTable()">
                 <b-row align-v="center" class="justify-content-md-center mt-3">
                     <recent-games-table :steamID="steamID" ref="RecentGamesTable" />
@@ -15,7 +20,7 @@
                 <b-row align-v="center" class="justify-content-md-center mt-3">
                     <players-games-table :steamID="steamID" ref="PlayersGamesTable"/>
                 </b-row>
-            </b-tab>
+            </b-tab>            
         </b-tabs>
     </b-container>
 </template>
@@ -24,12 +29,14 @@
 import MainCard from '../../components/user/card/MainCard.vue';
 import RecentGamesTable from '../../components/user/tables/RecentGamesTable.vue';
 import PlayersGamesTable from '../../components/user/tables/PlayersGamesTable.vue';
+import CalculatorCard from '../../components/calculator/CalculatorCard.vue';
 
 export default {
     middleware: ['auth_steam'],
-    components: { 
+    components: {
         PlayersGamesTable,
         RecentGamesTable,
+        CalculatorCard,
         MainCard, 
     },
     data() {
